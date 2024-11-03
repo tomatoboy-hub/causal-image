a1b8c858a2a4b955853281bb5eb5b0b108b446b1
tomatoboy-hub <hayatarou.ay@gmail.com>
2024-11-03 13:00:10 +0000

    [add] try multi seeds

--- run/run.sh
+++ run/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # 環境変数の設定（必要に応じて）
 export CUDA_VISIBLE_DEVICES=0
 
@@ -8,6 +7,8 @@ export CUDA_VISIBLE_DEVICES=0
 pretrained_models=("timm/eva02_tiny_patch14_224.mim_in22k" "timm/efficientvit_m2.r224_in1k" "timm/vit_base_patch32_clip_224.laion2b_ft_in12k_in1k")
 batch_sizes=(32)
 epochs=(3)
+seeds=(42 123 456 789 1011 1213 1415 1617 1819 2021 2223 2425 2627 2829 3031 3233 3435 3637 3839 4041)  # シード値のリスト
+
 
 # 初期のexp_idを設定
 exp_id=1
@@ -16,20 +17,21 @@ exp_id=1
 for model in "${pretrained_models[@]}"; do
     for batch_size in "${batch_sizes[@]}"; do
         for epoch in "${epochs[@]}"; do
+            for seed in "${seeds[@]}"; do
+                # Hydraの設定をオーバーライドして実行
+                python train.py \
+                    pretrained_model=$model \
+                    batch_size=$batch_size \
+                    epoch=$epoch \
+                    exp_id=$exp_id \
+                    seed=$seed
 
-            # Hydraの設定をオーバーライドして実行
-            python train.py \
-                pretrained_model=$model \
-                batch_size=$batch_size \
-                epoch=$epoch \
-                exp_id=$exp_id
-
-            # exp_idをインクリメント
-            exp_id=$((exp_id + 1))
-            
-            # 少し待機して次の実行（オプション）
-            sleep 5
-
+                # exp_idをインクリメント
+                exp_id=$((exp_id + 1))
+                
+                # 少し待機して次の実行（オプション）
+                sleep 5
+            done
         done
     done
 done
